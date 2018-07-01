6361495ad35aa4d309d6483420a69738b4b03546
tio-bryan <bryan.pesca@hotmail.com>
2018-07-01 01:21:22 -0300

    ajustes

--- ep_estatistica.py
+++ ep_estatistica.py
@@ -3,7 +3,7 @@
 
 #!/usr/bin/python
 
-import Tkinter as tk # Lib p/ criar janelas
+from Tkinter import * # Lib p/ criar janelas
 import tkMessageBox
 import matplotlib # Lib p/ plotar graficos
 
@@ -13,16 +13,13 @@ def microeconomia():
 def macroeconomia():
     tkMessageBox.showinfo('Macroeconomia', 'Macroeconomia')
 
-root = tk.Tk() # Cria menu principal
+root = Tk() # Cria menu principal
 root.title('EP de Estatística')
+root.geometry('250x250')
 
 # Cria botões
-microeconomia = tk.Button(root, text='Microeconomia', command=microeconomia)
-macroeconomia = tk.Button(root, text='Macroeconomia', command=macroeconomia)
-quit = tk.Button(root, text='Sair', fg='red', command=quit)
-
-microeconomia.pack()
-macroeconomia.pack()
-quit.pack()
+microeconomia = Button(root, text='Microeconomia', command=microeconomia).pack()
+macroeconomia = Button(root, text='Macroeconomia', command=macroeconomia).pack()
+Button(root, text='Sair', fg='red', command=quit).pack()
 
 root.mainloop()
